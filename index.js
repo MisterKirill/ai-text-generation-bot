65ddb0e0100c805344702c719b653481227ee982
Kirill <87936030+MisterKirill@users.noreply.github.com>
2023-01-14 23:54:35 +0500

    lol

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const { Client, GatewayIntentBits } = require('discord.js')
+const axios = require('axios').default
+
+const bot = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+bot.on('messageCreate', msg => {
+    if(msg.author.bot) return
+
+    if(msg.channelId == process.env.ALLOWED_CHANNEL && msg.content) {
+        console.log(`${msg.author.username} called text generation`)
+
+        axios.post('https://api-inference.huggingface.co/models/fossai/cheburek-davinci-1', JSON.stringify(msg.content), {
+            headers: {
+                'Authorization': `Bearer ${process.env.HF_TOKEN}`
+            }
+        }).then(res => {
+            msg.reply(res.data[0].generated_text)
+        }).catch(err => {
+            if(err.data.error) {
+                if(err.data.error == 'Model fossai/cheburek-davinci-1 is currently loading') {
+                    msg.reply(`Модель загружается, подождите ещё примерно ${Math.round(err.data.estimated_time)} секунд...`)
+                    return
+                }
+            }
+
+            console.log(err)
+            msg.reply('Ошибка апи hugging face напиши мисту')
+        })
+    }
+})
+
+bot.on('ready', () => {
+    console.log('bot ready')
+})
+
+bot.login(process.env.BOT_TOKEN)
