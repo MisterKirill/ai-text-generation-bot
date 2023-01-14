68495f6bfe13109cfe01da5bfdd88076d80226e1
Kirill <87936030+MisterKirill@users.noreply.github.com>
2023-01-15 01:41:15 +0500

    updates

--- index.js
+++ index.js
@@ -16,18 +16,16 @@ bot.on('messageCreate', msg => {
     if(msg.channelId == process.env.ALLOWED_CHANNEL && msg.content) {
         console.log(`${msg.author.username} called text generation`)
 
-        axios.post('https://api-inference.huggingface.co/models/fossai/cheburek-davinci-1', JSON.stringify(msg.content), {
+        axios.post(`https://api-inference.huggingface.co/models/${process.env.HF_MODEL}`, JSON.stringify(msg.content), {
             headers: {
                 'Authorization': `Bearer ${process.env.HF_TOKEN}`
             }
         }).then(res => {
             msg.reply(res.data[0].generated_text)
         }).catch(err => {
-            if(err.data.error) {
-                if(err.data.error == 'Model fossai/cheburek-davinci-1 is currently loading') {
-                    msg.reply(`Модель загружается, подождите ещё примерно ${Math.round(err.data.estimated_time)} секунд...`)
-                    return
-                }
+            if(err.data && err.data.estimated_time) {
+                msg.reply(`Модель загружается, подождите ещё примерно ${Math.round(err.data.estimated_time)} секунд...`)
+                return
             }
 
             console.log(err)
